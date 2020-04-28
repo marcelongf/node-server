2cc34f0d20b0c9efe149df2f21cd5678ea628269
Marcelo Nassar <mfilho782@beijaflore.com>
2020-04-28 12:20:15 -0300

    first changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 
 const server = http.createServer(function(req, res) {
-    console.log(req);
+    console.log(req.url, req.methods, req.headers);
 });
 
 server.listen(3000);
